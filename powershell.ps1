d205c26f09abe00d436d34078a186494f34e0dd3
github-cloudlabsuser-1964 <github_cloudlabsuser_1964@cloudlabsaiuser.com>
2024-06-04 19:18:01 +0000

    copilot commit 4

---
+++ powershell.ps1
@@ -0,0 +1,19 @@
+# install the Az module if not already installed
+Install-Module -Name Az -AllowClobber -Scope CurrentUser
+
+
+
+
+# Login to Azure
+Connect-AzAccount
+
+# Set variables
+$resourceGroupName = "myResourceGroup"
+$location = "West Europe"
+$storageAccountName = "mystorageaccount"
+
+# Create a resource group
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create a storage account
+New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName Standard_LRS
